1367ea516a69e0fa299b6921a1a048ea8c96447d
Aaron Helton <mariusagricola@gmail.com>
2016-05-06 15:24:57 +0000

    fix premature settings

--- skjold/settings.py
+++ skjold/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dal',
-    'dal_select2',
+    #'dal',
+    #'dal_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
