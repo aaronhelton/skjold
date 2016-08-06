dca7f74ea9a7a63dbdecbdcf54b32957a4882b08
Aaron Helton <abhelton@gmail.com>
2016-08-06 09:17:43 -0400

    fix volumes for collectstatic

--- skjold/settings.py
+++ skjold/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from rdflib import ConjunctiveGraph
 from rdflib_sqlalchemy.SQLAlchemy import SQLAlchemy
